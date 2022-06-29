f6ee91c8f79c2a6df74fe9ff96ac6eccefe9cb44
Alex Petrov <107173742+AlexPetrov8712@users.noreply.github.com>
2022-06-29 13:02:15 +0300

    Add files via upload

---
+++ Vowels Sum.py
@@ -0,0 +1,14 @@
+line = input()
+sums = 0
+for i in line:
+    if i == 'a':
+        sums += 1
+    if i == 'e':
+        sums += 2
+    if i == 'i':
+        sums += 3
+    if i == 'o':
+        sums += 4
+    if i == 'u':
+        sums += 5
+print(sums)
