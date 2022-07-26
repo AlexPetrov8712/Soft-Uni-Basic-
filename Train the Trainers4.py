79104b4d800295114cdda22b698d0a9c3d935829
Alex Petrov <107173742+AlexPetrov8712@users.noreply.github.com>
2022-07-26 16:02:03 +0300

    Add files via upload

---
+++ Train the Trainers4.py
@@ -0,0 +1,11 @@
+n = int(input())
+sums = 0
+for i in range(n):
+    all_point = 0
+    point = 0
+    command = input()
+    while command != 'Finish':
+        point = int(command)
+        all_point += point
+
+
